d3659840e9e1f7ca72081c3c09d1a9c15f12b6bc
Samira <samira.rashid@transcombd.com>
2023-09-18 17:17:20 +0600

    Auth part

--- auth/lib/src/infra/api/auth_api.dart
+++ auth/lib/src/infra/api/auth_api.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:async/async.dart';
+import 'package:auth/src/infra/api/mapper.dart';
 import 'package:http/http.dart' as http;
 import 'package:auth/src/domain/credential.dart';
 import 'package:auth/src/infra/api/auth_api_contract.dart';
@@ -9,14 +12,25 @@ class AuthApi implements IAuthAPI {
 
   AuthApi(this.baseUrl, this._client);
   @override
-  Future<Result<String>> signIn(Credential credential) {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<Result<String>> signIn(Credential credential) async {
+    var endpoint = '$baseUrl/auth/signin';
+    return await _postCredential(endpoint, credential);
   }
 
   @override
-  Future<Result<String>> signUp(Credential credential) {
-    // TODO: implement signUp
-    throw UnimplementedError();
+  Future<Result<String>> signUp(Credential credential) async {
+    var endpoint = '$baseUrl/auth/signup';
+    return await _postCredential(endpoint, credential);
+  }
+
+  Future<Result<String>> _postCredential(
+      String endpoint, Credential credential) async {
+    var response = await _client.post(Uri.parse(endpoint),
+        body: Mapper.toJson(credential));
+    // if(response)
+    var json = jsonDecode(response.body);
+    return json['auth_token'] != null
+        ? Result.value(json['auth_token'])
+        : Result.error(json['message']);
   }
 }
